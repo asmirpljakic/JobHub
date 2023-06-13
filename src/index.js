57d57984177e1a054290844ca3335671624c9c64
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-13 09:07:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,58 +1,16 @@
-//
-//
-//
-//
-//_______________________________425_____________________________
-//
-//
-//
-//
-
-// // Create New Applictaion
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// // 2: Brisemo/Zakometarisemo: import reportWebVitals from './reportWebVitals';
-// // import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// // 2:(KRAJ!) I ovo cemo sve IZBRISATI/ZAKOMENTARISATI:
-// /*
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-// */
-
-//
-//
-//
-//
-//_______________________________427_____________________________
-//
-//
-//
-//
-
-// Jobster - Title and Favicon
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//N 2: VAZNO: UVOZIMO GA PRE 'index.css'-a:   POSLE IDEMO u App.js    import 'normalize.css';
 import 'normalize.css';
 import './index.css';
 import App from './App';
+import { store } from './store';
+import { Provider } from 'react-redux';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );

35f471bc6e537ee90450d45d31da75d08339e74f
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-16 09:16:44 +0200

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import { Logo, FormRow } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 import { toast } from 'react-toastify';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser, registerUser } from '../features/user/userSlice';
 
 const initialState = {
   name: '',
@@ -10,10 +12,10 @@ const initialState = {
   password: '',
   isMember: true,
 };
-
 const Register = () => {
   const [values, setValues] = useState(initialState);
-
+  const { user, isLoading } = useSelector(store => store.user);
+  const dispatch = useDispatch();
   const handleChange = e => {
     const name = e.target.name;
     const value = e.target.value;
@@ -24,9 +26,14 @@ const Register = () => {
     const { name, email, password, isMember } = values;
     if (!email || !password || (!isMember && !name)) {
       toast.error('Please fill out all fields');
+      return;
     }
+    if (isMember) {
+      dispatch(loginUser({ email: email, password: password }));
+      return;
+    }
+    dispatch(registerUser({ name, email, password }));
   };
-
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
   };
@@ -34,7 +41,6 @@ const Register = () => {
     <Wrapper className='full-page'>
       <form className='form' onSubmit={onSubmit}>
         <Logo />
-
         <h3>{values.isMember ? 'Login' : 'Register'}</h3>
         {!values.isMember && (
           <FormRow

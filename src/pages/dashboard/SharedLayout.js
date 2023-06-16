e8af4b42f8a56b16d87706cd4b5c84f9d393e3cb
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-16 09:43:54 +0200

    add SharedLayout

---
+++ src/pages/dashboard/SharedLayout.js
@@ -0,0 +1,53 @@
+//
+//
+//
+//
+//
+//
+// __________________________ 456 __________________________
+//
+//
+//
+//
+//
+//
+
+// // Jobster - Setup Dashboard Pages
+
+// // 7: Kucamo skracenicu: 'rafce'
+
+// import React from 'react';
+
+// const SharedLayout = () => {
+//   return (
+//     //N 7: <h1>SharedLayout</h1> IDEMO u index.js(dashboard)
+//     <h1>SharedLayout</h1>
+//   );
+// };
+
+// export default SharedLayout;
+
+//
+//
+//
+//
+//
+//
+// __________________________ 457 __________________________
+//
+//
+//
+//
+//
+//
+
+// Jobster - Complete SharedLayout
+
+import React from 'react';
+// 6
+
+const SharedLayout = () => {
+  return <h1>SharedLayout</h1>;
+};
+
+export default SharedLayout;

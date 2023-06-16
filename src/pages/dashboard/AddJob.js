c71f35101ff5bdbf54a1174520cd99e1616d7577
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-16 09:44:18 +0200

    alljobs and addjob

---
+++ src/pages/dashboard/AddJob.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const AddJob = () => {
+  return <h1>AddJob</h1>;
+};
+export default AddJob;
---
+++ src/pages/dashboard/AllJobs.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const AllJobs = () => {
+  return <h1>AllJobs</h1>;
+};
+export default AllJobs;

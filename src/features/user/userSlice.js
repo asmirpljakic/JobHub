878b20e6182b063ddeae623cb61771455450fd06
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-16 09:24:00 +0200

    Update userSlice.js

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,14 +1,21 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
+import customFetch from '../../utils/axios';
 
 const initialState = {
   isLoading: false,
   user: null,
 };
+
 export const registerUser = createAsyncThunk(
   'user/registerUser',
   async (user, thunkAPI) => {
-    console.log(`Register User ${JSON.stringify(user)}`);
+    try {
+      const resp = await customFetch.post('/auth/testingRegister', user);
+    } catch (error) {
+      toast.error(error.response.data.msg);
+      console.log(error.response);
+    }
   }
 );
 export const loginUser = createAsyncThunk(
@@ -21,4 +28,5 @@ const userSlice = createSlice({
   name: 'user',
   initialState,
 });
+
 export default userSlice.reducer;

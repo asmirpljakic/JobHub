b76892ebe54546aa171ba4e89bdae4eeca7242ea
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-16 09:26:22 +0200

    Update userSlice.js

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -11,22 +11,39 @@ export const registerUser = createAsyncThunk(
   'user/registerUser',
   async (user, thunkAPI) => {
     try {
-      const resp = await customFetch.post('/auth/testingRegister', user);
+      const resp = await customFetch.post('/auth/register', user);
+      return resp.data;
     } catch (error) {
-      toast.error(error.response.data.msg);
-      console.log(error.response);
+      return thunkAPI.rejectWithValue(error.response.data.msg);
     }
   }
 );
+
 export const loginUser = createAsyncThunk(
   'user/loginUser',
   async (user, thunkAPI) => {
     console.log(`Lgoin User ${JSON.stringify(user)}`);
   }
 );
+
 const userSlice = createSlice({
   name: 'user',
   initialState,
+  extraReducers: {
+    [registerUser.pending]: state => {
+      state.isLoading = true;
+    },
+    [registerUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload;
+      state.isLoading = false;
+      state.user = user;
+      toast.success(`Hello There ${user.name}`);
+    },
+    [registerUser.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+  },
 });
 
 export default userSlice.reducer;

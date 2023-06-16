38de4bdaf7e5e2042bbccde0908a3ed69885b039
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-16 09:32:01 +0200

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -5,6 +5,7 @@ import Wrapper from '../assets/wrappers/RegisterPage';
 import { toast } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginUser, registerUser } from '../features/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 const initialState = {
   name: '',
@@ -16,8 +17,8 @@ const initialState = {
 const Register = () => {
   const [values, setValues] = useState(initialState);
   const { user, isLoading } = useSelector(store => store.user);
-
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleChange = e => {
     const name = e.target.name;
     const value = e.target.value;
@@ -40,11 +41,19 @@ const Register = () => {
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
   };
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate('/');
+      }, 2000);
+    }
+  }, [user]);
+
   return (
     <Wrapper className='full-page'>
       <form className='form' onSubmit={onSubmit}>
         <Logo />
-
         <h3>{values.isMember ? 'Login' : 'Register'}</h3>
         {!values.isMember && (
           <FormRow

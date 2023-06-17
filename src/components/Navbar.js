cb17095b582807ab5b33cbec638a649a04089e71
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-17 08:19:55 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/Navbar';
+import { FaHome } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <Wrapper>
       <h2>Navbar</h2>
+      <FaHome />
     </Wrapper>
   );
 };

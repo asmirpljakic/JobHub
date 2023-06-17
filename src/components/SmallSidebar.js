404428990ebe1a316b50c7141e56771d05952988
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-17 08:17:33 +0200

    Update SmallSidebar.js

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Wrapper from '../assets/wrappers/SmallSidebar';
 
 const SmallSidebar = () => {
-  return <h2>SmallSidebar</h2>;
+  return (
+    <Wrapper>
+      <h2>SmallSidebar</h2>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;

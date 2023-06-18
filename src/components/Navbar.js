14186953df60b15028d35dce87186f00a6779fcd
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-18 08:47:47 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { toggleSidebar } from '../features/user/userSlice';
 
 const Navbar = () => {
+  const [showLogout, setShowLogout] = useState(false);
   const { user } = useSelector(store => store.user);
   const dispatch = useDispatch();
-
   const toggle = () => {
     dispatch(toggleSidebar());
   };
@@ -28,13 +28,13 @@ const Navbar = () => {
           <button
             type='button'
             className='btn'
-            onClick={() => console.log('toggle logout dropdown')}
+            onClick={() => setShowLogout(!showLogout)}
           >
             <FaUserCircle />
             {user?.name}
             <FaCaretDown />
           </button>
-          <div className='dropdown show-dropdown'>
+          <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
             <button
               type='button'
               className='dropdown-btn'

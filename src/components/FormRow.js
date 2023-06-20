79591e2551407f06b7beaafe0b867d4f38df3712
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-20 07:54:11 +0200

    Update FormRow.js

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -1,9 +1,10 @@
 import React from 'react';
+
 const FormRow = ({ type, name, value, handleChange, labelText }) => {
   return (
     <div className='form-row'>
       <label htmlFor={name} className='form-label'>
-        {name}
+        {labelText || name}
       </label>
       <input
         type={type}

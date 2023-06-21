104bd28b1f7c988f3c86cd99c853137173447a36
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-21 08:22:24 +0200

    Update userSlice.js

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -49,7 +49,10 @@ export const updateUser = createAsyncThunk(
       });
       return resp.data;
     } catch (error) {
-      console.log(error.response);
+      if (error.response.status === 401) {
+        thunkAPI.dispatch(logoutUser());
+        return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
+      }
 
       return thunkAPI.rejectWithValue(error.response.data.msg);
     }

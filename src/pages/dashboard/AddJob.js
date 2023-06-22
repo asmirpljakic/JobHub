5c9545059efa1ee6c40b3452b78fb42e764ec209
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-22 08:23:21 +0200

    Update AddJob.js

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { FormRow } from '../../components';
+
+import { FormRow, FormRowSelect } from '../../components';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 import { useSelector, useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
+import { handleChange } from '../../features/job/jobSlice';
 
 const AddJob = () => {
   const {
@@ -17,18 +19,21 @@ const AddJob = () => {
     isEditing,
     editJobId,
   } = useSelector(store => store.job);
+  const dispatch = useDispatch();
 
   const handleSubmit = e => {
     e.preventDefault();
+
     if (!position || !company || !jobLocation) {
       toast.error('Please fill out all fields!');
       return;
     }
   };
+
   const handleJobInput = e => {
     const name = e.target.name;
     const value = e.target.value;
-    console.log(name, value);
+    dispatch(handleChange({ name, value }));
   };
 
   return (
@@ -36,24 +41,43 @@ const AddJob = () => {
       <form className='form'>
         <h3>{isEditing ? 'edit job' : 'add job'}</h3>
         <div className='form-center'>
+          {/* position */}
           <FormRow
             type='text'
             name='position'
             value={position}
             handleChange={handleJobInput}
           />
+          {/* company */}
           <FormRow
             type='text'
             name='company'
             value={company}
             handleChange={handleJobInput}
           />
+          {/* jobLocation */}
           <FormRow
             type='text'
             name='jobLocation'
             labelText='job location'
             value={jobLocation}
             handleChange={handleJobInput}
+          />
+          {/* status */}
+          <FormRowSelect
+            name='status'
+            value={status}
+            handleChange={handleJobInput}
+            list={statusOptions}
+          />
+
+          {/* job type */}
+          <FormRowSelect
+            name='jobType'
+            labelText='job type'
+            value={jobType}
+            handleChange={handleJobInput}
+            list={jobTypeOptions}
           />
           <div className='btn-container'>
             <button

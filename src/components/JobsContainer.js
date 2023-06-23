2b5e671452942e7c28a472cfe6525feaadaa8d0f
Asmir Pljakic <asmirpljakic@gmail.com>
2023-06-23 08:24:40 +0200

    Update JobsContainer.js

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -4,12 +4,17 @@ import Job from './Job';
 import Wrapper from '../assets/wrappers/JobsContainer';
 import { useSelector, useDispatch } from 'react-redux';
 import Loading from './Loading';
+import { getAllJobs } from '../features/allJobs/allJobsSlice';
 
 const JobsContainer = () => {
   const { jobs, isLoading } = useSelector(store => store.allJobs);
   const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllJobs());
+  }, []);
+
   if (isLoading) {
-    return <Loading />;
+    return <Loading center />;
   }
   if (jobs.length === 0) {
     return (
@@ -24,7 +29,6 @@ const JobsContainer = () => {
       <h5>jobs info</h5>
       <div className='jobs'>
         {jobs.map(job => {
-          console.log(job);
           return <Job key={job._id} {...job} />;
         })}
       </div>
